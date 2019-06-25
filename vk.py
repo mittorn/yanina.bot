e8299afc4f3788816d96721a1a55238e1acc11e4
mittorn <mittorn@sibmail.com>
2019-06-25 21:57:17 +0000

    vk: add option to ignore exceptions

--- vk.py
+++ vk.py
@@ -3,25 +3,29 @@ import requests
 import time
 lp_modes = aenum('LP_PAGE','LP_GROUP')
 call_modes = aenum('CALL_NORMAL', 'CALL_AUDIO' ,'CALL_GROUP', 'CALL_BROWSER')
-api_versions = { CALL_NORMAL : '5.71', CALL_AUDIO : '5.71', CALL_GROUP : '5.50' }
+api_versions = { CALL_NORMAL : '5.95', CALL_AUDIO : '5.71', CALL_GROUP : '5.50' }
 api_tokens = {}
 
-def vk_call(mode,method,specparam):
-	param = {}
-	if mode == CALL_BROWSER:
-		raise Exception('Not implemented yet')
-	if mode in api_versions:
-		param['v'] = api_versions[mode]
-	if mode in api_tokens:
-		param['access_token'] = api_tokens[mode]
-	param.update(specparam)
-	headers = {}
-	if mode == CALL_AUDIO:
-		headers['User-Agent'] = 'VKAndroidApp/4.38-816 (Android 6.0; SDK 23; x86; Google Nexus 5X; ru)'
-	result = requests.post('https://api.vk.com/method/%s'%method, data=param, headers = {'User-Agent':'VKAndroidApp/4.38-816 (Android 6.0; SDK 23; x86; Google Nexus 5X; ru)'}).json()
-	if not 'response' in result:
-		raise Exception(result['error']['error_msg'])
-	return result['response']
+def vk_call(mode,method,specparam, ignore = False):
+	try:
+		param = {}
+		if mode == CALL_BROWSER:
+			raise Exception('Not implemented yet')
+		if mode in api_versions:
+			param['v'] = api_versions[mode]
+		if mode in api_tokens:
+			param['access_token'] = api_tokens[mode]
+		param.update(specparam)
+		headers = {}
+		if mode == CALL_AUDIO:
+			headers['User-Agent'] = 'VKAndroidApp/4.38-816 (Android 6.0; SDK 23; x86; Google Nexus 5X; ru)'
+		result = requests.post('https://api.vk.com/method/%s'%method, data=param, headers = {'User-Agent':'VKAndroidApp/4.38-816 (Android 6.0; SDK 23; x86; Google Nexus 5X; ru)'}).json()
+		if not 'response' in result:
+			raise Exception(result['error']['error_msg'])
+		return result['response']
+	except Exception as e:
+		if not ignore:
+			raise e
 
 def _load_config():
 	global config
