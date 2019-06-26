10757e05929cfdb1b8542842f2478538640f3d61
mittorn <mittorn@sibmail.com>
2019-06-26 17:20:46 +0000

    Fix utf-8 and module reloading

--- bot_commands/main.py
+++ bot_commands/main.py
@@ -2,8 +2,14 @@
 
 def help(p,text,msg):
 	"cmd помощь,help Список комманд"
-	vk_send(p,'Реализовано:\n'+('\n'.join(cmd_helps)))
+	vk_send(p,u'Реализовано:\n'+('\n'.join(cmd_helps)))
 
 def test(p,t,m):
 	"cmd тест Просто тест"
 	vk_send(p,'тест')
+
+def reload(p,t,m):
+	"cmd reload"
+	print 'перезагрузка команд'
+	reload_commands()
+	print 'done'
--- command.py
+++ command.py
@@ -1,6 +1,6 @@
 # coding: utf-8
 import os, sys, types
-
+import imp
 cmd_commands = {}
 cmd_helps = []
 cmd_modules = []
@@ -15,17 +15,31 @@ def force_utf8():
 #force_utf8()
 #print sys.getdefaultencoding()
 cmd_imports = None
+cmd_instance = 0
 
 def load_commands():
-	global cmd_modules, cmd_commands, cmd_helps, cmd_imports
+	global cmd_modules, cmd_commands, cmd_helps, cmd_imports, cmd_instance
+	cmd_instance += 1
 	if not cmd_imports:
 		cmd_imports = __import__('command_imports')
-	p = sys.path
-	sys.path = ['bot_commands/']
-	cmd_modules = [__import__(module[0:-3]) for module in filter(lambda x: x and x[-3:] == ".py", os.listdir("bot_commands"))]
-	sys.path = p
+#	p = sys.path
+#	sys.path = ['bot_commands/']
+	for modname in filter(lambda x: x and x[-3:] == ".py", os.listdir("bot_commands")):
+		module = imp.load_source('cmd_'+str(cmd_instance)+'_'+modname[0:-3],'bot_commands/'+modname)
+#		reload(module)
+#		del module
+#		del sys.modules[modname[0:-3]]
+#		del sys.modules[modname[0:-3]]
+#		try:
+#			reload(module)
+#		except Exception:
+#			pass
+#		module = __import__(modname[0:-3])
+		cmd_modules.append(module)
+#	sys.path = p
 
 	for m in cmd_modules:
+		print m.__name__, m
 		v = m.__dict__
 		for d in v:
 			f = v[d]
@@ -33,35 +47,54 @@ def load_commands():
 				continue
 			if not hasattr(f,'__doc__'):
 				continue
-			doc = f.__doc__.split(' ')
-#			print doc
+			if not f.__doc__:
+				continue
+			doc = f.__doc__.decode('utf-8').split(' ')
+			print doc
 			if doc[0] != 'cmd':
 				continue
 			l = doc[1].split(',')
 			for c in l:
 				cmd_commands[c] = f
-			cmd_helps.append(', '.join(l) + ' - ' + (' '.join(doc[2:])))
+			if len(doc) > 2:
+				cmd_helps.append(', '.join(l) + ' - ' + (' '.join(doc[2:])))
 #		print vk.__dict__
-		m.__dict__.update(cmd_imports.__dict__)
+		d1 = {}
+		d1.update(cmd_imports.__dict__)
+		d1.update(m.__dict__)
+		m.__dict__.update(d1)
 #		print vk.__dict__['cmd']
 
 def unload_commands():
 	global cmd_commands, cmd_modules, cmd_helps, cmd_imports
-	cmd_commands = {}
-	cmd_helps = []
+	cmd_commands.clear()
+	del cmd_helps[:]
+#	p = sys.path
+#	sys.path = ['bot_commands/']
 	for module in cmd_modules:
-		for d in cmd_imports.__dict__:
-			if d in module.__dict__:
-				module.__dict__.pop(d)
+#		for d in cmd_imports.__dict__:
+#			if d in module.__dict__:
+#				module.__dict__.pop(d)
+#		reload(module)
+		n = '' + module.__name__
+		shit = []
+		for d in module.__dict__:
+			shit.append(d)
+		for s in shit:
+			module.__dict__.pop(s)
+		del sys.modules[n]
 		del module
-	cmd_modules = []
+#		sys.modules.pop(n)
+	del cmd_modules[:]
+#	sys_path = p
+#	cmd_modules = []
 
 def reload_commands():
 	unload_commands()
 	load_commands()
 
 def handle_command(peer,cmd,text,msg):
-	cmd = cmd.encode('utf-8')
+	cmd = cmd
 
 	if cmd in cmd_commands:
 		cmd_commands[cmd](peer,text,msg)
