41e48a827789e2bfa189f7c2844d2f8391916f82
mittorn <mittorn@sibmail.com>
2019-07-25 03:40:34 +0000

    im: auth support

--- im.py
+++ im.py
@@ -2,9 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import sys
+from utils import *
 from vk import config
 import json
 import requests
+import re
+
+imconfig = {'remixsid':''}
+try:
+	imconfig.update(load_object('imconfig'))
+except:
+	pass
+
 USER_AGENT = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.360"
 # mailru chat flags
 MAIL_CHAT_FLAG_ADMINS_CAN_INVITE_LINK = 32
@@ -12,10 +21,68 @@ MAIL_CHAT_FLAG_ADMINS_CAN_ADD_ADMINS = 16
 MAIL_CHAT_FLAG_ONLY_ADMINS_CAN_CHANGE_TITLE = 8
 MAIL_CHAT_FLAG_ONLY_ADMINS_CAN_INVITE = 1
 MAIL_CHAT_FLAG_ONLY_ADMINS_CAN_PIN = 4
+REAUTH_CODE=u'99b478ae0183d739b8'
+class InvalidAuthException(Exception):
+	pass
+class ReauthException(Exception):
+	pass
+
+def login(login, password):
+    result = None
+
+    ip_h, lg_h, cookie = __get_h()
+    remixlhk = re.findall('remixlhk=(?P<h>[A-Za-z0-9]+);', cookie)[1]
+    remixstid = re.search('remixstid=(?P<h>[A-Za-z0-9_]+);', cookie).group('h')
+
+    payload = {
+        "act": "login",
+        "role": "al_frame",
+        "expire": "",
+        "recapthca":"",
+        "captcha_key": "",
+        "captcha_sid": "",
+        "_origin": "https://vk.com",
+        "ip_h": ip_h,
+        "lg_h": lg_h,
+        "email": login,
+        "pass": password,
+        "utf-8": "1"
+    }
+    headers = {
+        "Referer": "https://vk.com",
+        "Origin": "https://vk.com",
+        "Accept-Charset": "utf-8",
+        "Cookie": "remixlang=0; remixlhk=%s; remixstid=%s; remixdt=0; remixflash=18.0.0; remixscreen_depth=24" % (remixlhk, remixstid),
+        "User-Agent":USER_AGENT
+    }
+    
+    r = requests.post("https://login.vk.com/?act=login", data=payload, headers=headers)
+    r.raise_for_status()
+
+    if "Set-Cookie" not in r.headers:
+        raise InvalidAuthException("No Cookie")
+    if r.status_code == 200:
+        result = re.search('remixsid=([A-Za-z0-9]+)', r.headers["Set-Cookie"]).group().split("=")[1]
+        if  result == "DELETED":
+            raise InvalidAuthException("200: DELETED Session")
+    else:
+        raise InvalidAuthException("NON 200 CODE")
+    return result
+
+
+def __get_h():
+    r = requests.get("https://vk.com/login")
+    r.raise_for_status()
+
+    ip_h = re.search('"ip_h" value="(?P<h>[A-Za-z0-9]+)"', r.text)
+    lg_h = re.search('"lg_h" value="(?P<h>[A-Za-z0-9]+)"', r.text)
+
+    return ip_h.group("h"), lg_h.group("h"), r.headers["Set-Cookie"]
+
 def im_post(url, payload, headers = {}):
         headers_original = {
             "Origin": "https://vk.com",
-            "Cookie" : "remixsslsid=1; remixsid=%s" % config['remixsid'],
+            "Cookie" : "remixsslsid=1; remixsid=%s" % imconfig['remixsid'],
             "Content-Type": "application/x-www-form-urlencoded",
             "Accept-Encoding": "gzip, deflate",
             "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4",
@@ -26,6 +93,9 @@ def im_post(url, payload, headers = {}):
         headers_original.update(headers)
         r = requests.post(url, data=payload, headers=headers_original)
         r.raise_for_status()
+        print(r.headers, r.text)
+        r2 = requests.get('https://vk.com', headers=headers_original)
+        print(r2.headers)
         return r
 
 def im_parse(s):
@@ -38,6 +108,8 @@ def im_parse(s):
 			ex  = spl[5].decode('utf-8')
 		except Exception:
 			pass
+		if ex == REAUTH_CODE:
+			raise ReauthException()
 		raise Exception(ex)
 	base = spl[5]
 	if base[0] == '<':
@@ -53,18 +125,28 @@ def im_parse(s):
 
 
 def im_request(chat_id, params):
-    add = {"al":1,"hash":get_chat_hash(chat_id),"im_v":2,"gid":0}
-    params.update(add)
+	add = {"al":1,"hash":get_chat_hash(chat_id),"im_v":2,"gid":0}
+	params.update(add)
 #    print add
-    return im_parse(im_post("https://vk.com/al_im.php",params).text)
+	try:
+		return im_parse(im_post("https://vk.com/al_im.php",params).text)
+	except ReauthException:
+		imconfig['remixsid'] = login(config['login'],config['password'])
+		save_object('imconfig',imconfig)
+		im_request(chat_id,params)
 
 def add_chat_bot(chat_id, bot_id):
-    r = im_post("https://vk.com/al_groups.php",{"act":"a_search_chats_box","al":1,"group_id":-bot_id})
-    start = r.text.find('add_hash')+11
-    l = r.text[start:].find('"')
-    hash = r.text[start:start+l]
-    r = im_post("https://vk.com/al_im.php",{"act":"a_add_bots_to_chat","al":1, "add_hash":hash,"bot_id":bot_id,"peer_ids":chat_id})
-    return im_parse(r.text)
+	r = im_post("https://vk.com/al_groups.php",{"act":"a_search_chats_box","al":1,"group_id":-bot_id})
+	start = r.text.find('add_hash')+11
+	l = r.text[start:].find('"')
+	hash = r.text[start:start+l]
+	r = im_post("https://vk.com/al_im.php",{"act":"a_add_bots_to_chat","al":1, "add_hash":hash,"bot_id":bot_id,"peer_ids":chat_id})
+	try:
+		return im_parse(r.text)
+	except ReauthException:
+		imconfig['remixsid'] = login(config['login'],config['password'])
+		save_object('imconfig',imconfig)
+		add_chat_bot(chat_id,bot_id)
 #    print r.text.encode('utf-8')
 
 def get_chat_hash(chat_id):
